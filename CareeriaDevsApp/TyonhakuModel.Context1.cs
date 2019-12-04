cef698689f1eb4cd655cab4bd7fda0f95e6059e2
Ville Siivonen <zyxelwill@hotmail.com>
2019-12-04 11:06:57 +0200

    Ja viestintäpalvelu olkaa hyvät.

--- CareeriaDevsApp/TyonhakuModel.Context1.cs
+++ CareeriaDevsApp/TyonhakuModel.Context1.cs
@@ -36,8 +36,5 @@ namespace CareeriaDevsApp
         public virtual DbSet<Viesti> Viesti { get; set; }
         public virtual DbSet<Yritys> Yritys { get; set; }
         public virtual DbSet<database_firewall_rules> database_firewall_rules { get; set; }
-        public virtual DbSet<Message> Message { get; set; }
-        public virtual DbSet<User> User { get; set; }
-        public virtual DbSet<UserConnection> UserConnection { get; set; }
     }
 }
