1deec30ec26537c929e768b220e2f0becd8d8f5c
Jarkko Haikarainen <jarkko.haikarainen@student.careeria.fi>
2019-12-11 12:18:47 +0200

    DataBaseModelin päivitystä

--- CareeriaDevsApp/TyonhakuModel1.Designer.cs
+++ CareeriaDevsApp/TyonhakuModel1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Careeria\Scrum ja ohjelmistotuotannon menetelmät\Scrum projekti\CareeriaDevsApp\CareeriaDevsApp\TyonhakuModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jarkk\source\repos\CareeriaDevsApp2\CareeriaDevsApp\TyonhakuModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
